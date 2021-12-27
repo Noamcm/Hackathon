981a6a6534392199da304aede5f3b4c46514d099
Noam <noam.cohen.m@gmail.com>
2021-12-27 17:24:46 +0200

    threads

--- Client.py
+++ Client.py
@@ -1,1 +1,10 @@
 import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((socket.gethostname(),2068))
+message="Client started, listening for offer requests...\n"
+client.send(message.encode())#,(serverName, serverPort))
+while True:
+    from_server = client.recv(4096)
+    print(from_server.decode("utf-8"))
+client.close()
