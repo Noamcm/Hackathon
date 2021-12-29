a342e21b12f56797bcf5387095674ae2cfa23866
Noamcm <noam.cohen.m@gmail.com>
2021-12-29 13:48:56 +0200

    error handling and comments

--- Hack_Client2.py
+++ Hack_Client2.py
@@ -4,53 +4,73 @@ import scapy.all as sc
 import sys, errno
 from getch import getch, getche
 
-
 my_name = "Shiri\n"
 
-
 def search_offer():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    s.bind(('',13117))
-    print("Client started, listening for offer requests...")
-    curr_port=0
-    curr_ip=0
-    while curr_port!=2068: #True: ???
-        try:
-            packet = s.recvfrom(13117)  
-            if str(packet[0]).startswith(r"b'\xab\xcd\xdc\xba\x02"):
-                unpacked = struct.unpack('>IbH',packet[0])
-                curr_port=unpacked[2]
-                curr_ip=packet[1][0]
-        except struct.error as e:
-            print("recieved error: ("+str(e)+") from port: " +str(curr_port) ) ##?????????
-            continue
-    connecting_to_server(curr_ip,curr_port)
+    #this function searches for server's offer
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        s.bind(('',13117))
+        print("Client started, listening for offer requests...")
+        curr_port=0
+        curr_ip=0
+        while curr_port!=2068: #True: ???
+            try:
+                packet = s.recvfrom(13117)  #recieve an offer
+                if str(packet[0]).startswith(r"b'\xab\xcd\xdc\xba\x02"): #checks Magic Cookie and Message type prefix
+                    unpacked = struct.unpack('>IbH',packet[0])
+                    curr_port=unpacked[2]
+                    curr_ip=packet[1][0]
+            except struct.error as e:
+                print("recieved error: ("+str(e)+") from port: " +str(curr_port) ) ##?????????
+                continue
+    except socket.error as er:
+        print("recieved error: (" + str(er) + ") in search offer")
+        return
+    connecting_to_server(curr_ip,curr_port) #connect to tcp...
 
 def connecting_to_server(ip,port):
+    #this function connects the client and the server with TCP connection
     print("Received offer from "+str(ip)+", attempting to connect...")
-    tcp_server= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host=socket.gethostbyname(socket.gethostname())
-    tcp_server.connect((host,port))
-    tcp_server.sendto(my_name.encode(), (host,port))
-    while True:
-        try:
-            data = str(tcp_server.recv(1024).decode())
-            print(data)
-            if data.startswith("Welcome"):
-                gameMode(tcp_server)
-                break
-        except OSError as e:
-            print(str(e))
-            pass
+    try:
+        tcp_server= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host=socket.gethostbyname(socket.gethostname())
+        tcp_server.connect((host,port))
+    except socket.error as er:
+        print("recieved error: (" + str(er) + ") in connecting to tcp server")
+        return
+    try:
+        tcp_server.sendto(my_name.encode(), (host,port)) #send client's name to the server 
+    except IOError as er:
+        if er.errno == errno.EPIPE:
+            print("recieved error: (" + str(er) + ") in sending data to server with tcp server")
+        return
+    try:
+        data = str(tcp_server.recv(1024).decode()) #recieve welcome to game message
+    except socket.error as er:
+        print("recieved error: (" + str(er) + ") in recieving data from server with tcp server")
+        return
+    print(data)
+    if data.startswith("Welcome"): 
+        gameMode(tcp_server) #start game...
         
 def gameMode(tcp_server):
-    char = getche()
-    tcp_server.sendto(char.encode(),tcp_server.getpeername())
-    data = str(tcp_server.recv(1024).decode())
+    #this function contains the fuctionality of the client's game mode
+    char = getche() #get the first char that was written, without enter being pressed
+    try:
+        tcp_server.sendto(char.encode(),tcp_server.getpeername()) #send the answer to the server
+    except IOError as er:
+        if er.errno == errno.EPIPE:
+            print("recieved error: (" + str(er) + ") in sending data to server in game mode")
+    try:
+        data = str(tcp_server.recv(1024).decode()) #get server's results of the game
+    except socket.error as er:
+        print("recieved error: (" + str(er) + ") in recieving data from server in game mode")
+        return
     print(data)
-
-
+    print("Server disconnected, listening for offer requests...")
 
 #main:
-search_offer()
+while True: #so the client will get more offers after finishing a game with a server
+    search_offer()
--- Hack_Server.py
+++ Hack_Server.py
@@ -8,6 +8,7 @@ import concurrent.futures
 import select
 import struct
 import scapy.all
+import random
 
 
 class Player:
@@ -17,104 +18,180 @@ class Player:
         self.address = address
         self.client = client
 
-port=2068
-host=socket.gethostbyname(socket.gethostname())
-udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-udp_server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #to avoid OSError: [Errno 98] Address already in use
-tcp_server.bind((host,port))
-player_num=0
-players=[]
-final_Message=""
-has_answer=False
-
 def main():
-    while True: 
-        #players=[]
-        global player_num
-        global players   
-        my_threads=[]
+    '''
+    this is the main function.
+    splits the server to three threads. 
+    one of them is responsible for sending broadcast udp packets with the tcp address
+    and two of them responsible for communicating with two clients (one for each)
+    
+    this function will loop as long as the app is running
+    '''
+    global qa
+    try:
+        qa = {"How much is 2+2":"4","How much is 0:3":"0","( 2 , 4 , 6 , _ ) -> What comes next":"8"}
+        start_print=True
+        while True: 
+            #players=[] 
+            global host,port,udp_server,tcp_server,player_num,players,my_threads,final_Message,has_answer
+            host,port,udp_server,tcp_server,player_num,players,my_threads,final_Message,has_answer=init_servers()
+            #print("starting main")
+            q=get_random_q()
+            my_threads.append(Thread(target=search_two_clients,args=(start_print,)))
+            my_threads.append(Thread(target=connect_to_client ,args=(q,)))
+            my_threads.append(Thread(target=connect_to_client ,args=(q,)))
+            for t in my_threads:
+                t.start()
+            for t in my_threads:
+                t.join()
+            start_print=False
+            close_servers()
+    except socket.error as er:
+        print("recieved error: (" + str(er) + ") in main")
+        return
+
+
+ 
+def init_servers():
+    '''
+    this function is responsible for initiating all the variables for a new game process
+    '''
+    port=2068
+    host=socket.gethostbyname(socket.gethostname())
+    try:
+        udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        udp_server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    except socket.error as er:
+        print("recieved error: (" + str(er) + ") in init_servers - udp_server")
+        return
+    try:
+        tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #to avoid OSError: [Errno 98] Address already in use
+        tcp_server.bind((host,port))
         tcp_server.listen(2) 
-        print("starting main")
-        my_threads.append(Thread(target=connect_to_client, daemon=True))
-        my_threads.append(Thread(target=connect_to_client, daemon=True))
-        for t in my_threads:
-            t.start()
-        search_two_clients()
-        for t in my_threads:
-            t.join()
-        
+    except socket.error as er:
+        print("recieved error: (" + str(er) + ") in init_servers - tcp_server")
+        return
+    player_num=0
+    my_threads=[]
+    players=[]
+    final_Message=""
+    has_answer=False
+    return(host,port,udp_server,tcp_server,player_num,players,my_threads,final_Message,has_answer)
 
-def connect_to_client():
-    global player_num
-    global players
-    Client, address = tcp_server.accept() 
-    packet = Client.recv(1024).decode()
-    player_num+=1
-    p=Player(player_num,packet,address,Client)
-    players.append(p)
-    while player_num<2:
-        #print("waiting for second player...")
-        time.sleep(1)
-    starts_game(p)
+       
 
-def starts_game(player):
-    global players
-    global final_Message
-    global has_answer
-    time.sleep(3) #10 seconds timer until the game begins
-    Player_Message="Welcome to Quick Maths.\n"
-    for p in players:
-        Player_Message+="Player "+str(p.number)+": "+str(p.name)
-    Player_Message+="==\nPlease answer the following question as fast as you can:\nHow much is 2+2?\n"
-    player.client.send(Player_Message.encode())
-    #add 10 seconds timer for answer
-    #print("waiting for char....")
-    #time.sleep(10) #10 seconds timer until the game begins
-    readable, empty, empt = select.select([player.client], [], [] , 5 ) # wait just 5sec 
-    if not readable and not has_answer:
-        final_Message="Game over!\nThe correct answer was "+answer+"!\nThe game finished with a DRAW\n"
-    elif not has_answer:
-        has_answer=True
-        client = readable[0]
-        answer = client.recv(1024).decode()
-        print(player.name,answer)
-        if (answer=="4"):
-            final_Message="Game over!\nThe correct answer was "+answer+"!\nnCongratulations to the winner: "+player.name
+def close_servers():   
+    '''
+    this function is responsible for closing all the sockets when a game is over
+    '''
+    try: 
+        udp_server.close()
+    except socket.error as er:
+        print("recieved error: (" + str(er) + ") in closing udp_server")
+        return
+    try: 
+        tcp_server.close()
+    except socket.error as er:
+        print("recieved error: (" + str(er) + ") in closing tcp_server")
+        return
+
+
+def connect_to_client(q):
+    '''for connecting to a client through TCP connection and send and
+    this function is responsible receiving player name from the client, waiting for the second cliennt and sending them to the game.
+    '''
+    try:
+        global tcp_server,player_num,players
+        Client, address = tcp_server.accept() 
+        packet = Client.recv(1024).decode()
+        player_num+=1
+        p=Player(player_num,packet,address,Client)
+        players.append(p)
+        while player_num<2:
+            #print("waiting for second player...")
+            time.sleep(1)
+        starts_game(p,q)
+    except socket.error as er:
+        print("recieved error: (" + str(er) + ") in connect_to_client")
+        return
+
+def starts_game(player,q):
+    '''
+    this function will handle the game.it will send the playes the questions and wait for answer.
+    when the forst answer arrive it will announce the player about the winner.
+    '''
+    global players,final_Message,has_answer
+    try:
+        time.sleep(3) #10 seconds timer until the game begins
+        Player_Message="Welcome to Quick Maths.\n"
+        for p in players:
+            Player_Message+="Player "+str(p.number)+": "+str(p.name)
+        Player_Message+="==\nPlease answer the following question as fast as you can:\n"+q+"?\n"
+    except socket.error as er:
+        print("recieved error: (" + str(er) + ") in starts_game")
+        return
+    try:
+        player.client.send(Player_Message.encode())
+    except IOError as er:
+        if er.errno == errno.EPIPE:
+            print("recieved error: (" + str(er) + ") in communicating with tcp server")
+        return
+    try:
+        readable, empty, empt = select.select([player.client], [], [] , 10 ) # wait just 5sec 
+        print(has_answer)
+        print(readable)
+        if not readable and not has_answer:
+            final_Message="\nGame over!\nThe correct answer was "+qa.get(q)+"!\nThe game finished with a DRAW\n"
+        elif not has_answer:
+            has_answer=True
+            client = readable[0]
+            answer = client.recv(1024).decode()
+            #print(player.name,answer)
+            if (answer==qa.get(q)):
+                final_Message="\nGame over!\nThe correct answer was "+qa.get(q)+"!\nnCongratulations to the winner: "+player.name
+            else:
+                other_player = ""
+                for p in players:
+                    if p.name!=player.name:
+                        other_player = p.name
+                final_Message="\nGame over!\nThe correct answer was "+qa.get(q)+"!\nCongratulations to the winner: "+ other_player
+
+    except socket.error as er:
+        print("recieved error: (" + str(er) + ") in starts_game")
+        return
+    try:
+        player.client.send(final_Message.encode())
+    except IOError as er:
+        if er.errno == errno.EPIPE:
+            print("recieved error: (" + str(er) + ") in communicating with tcp server")
+        return
+
+def search_two_clients(boolean):
+    '''
+    this function is responsible for sending UDP packets through the udp server until there will be 2 players.
+    '''
+    global host,port,udp_server,player_num
+    try:
+        if boolean:
+            print("Server started, listening on IP address "+str(host))
         else:
-            other_player = ""
-            for p in players:
-                if p.name!=player.name:
-                    other_player = p.name
-            final_Message="Game over!\nThe correct answer was "+answer+"!\nCongratulations to the winner: "+ other_player
-    player.client.send(final_Message.encode())
+            print("Game over, sending out offer requests...")
+        # socket.AF_INET -> ask for protocol IPv4
+        # socket.SOCK_STREAM -> ask for TCP connection
+        message = struct.pack('>IbH',0xabcddcba,0x2,port)  #Magic cookie (4 bytes): 0xabcddcba
+        while player_num<2: #TO CHANGE
+            udp_server.sendto(message, ('<broadcast>', 13117)) 
+            #print("message sent!")
+            time.sleep(1)
+    except socket.error as er:
+        print("recieved error: (" + str(er) + ") in search_two_clients")
+        return
 
-'''
-def timer():
-    global my_threads
-    while player_num<2:
-        time.sleep(1)
-    while player_num==2:
-        print("in while 1")
-        time.sleep(5) #the game
-        #now 10 sec is over
-        print("GAME OVER")
-        for t in my_threads:
-            t.stop()
-
-'''
-
-def search_two_clients():
-    global player_num
-    timeout = time.time() + 60*2   # 5 minutes from now
-    print("Server started, listening on IP address "+str(host))
-    # socket.AF_INET -> ask for protocol IPv4
-    # socket.SOCK_STREAM -> ask for TCP connection
-    message = struct.pack('>IbH',0xabcddcba,0x2,port)  #Magic cookie (4 bytes): 0xabcddcba
-    while player_num<2: #TO CHANGE
-        udp_server.sendto(message, ('<broadcast>', 13117)) 
-        #print("message sent!")
-        time.sleep(1)
+def get_random_q():
+    global qa
+    random_num = random.randint(0,len(qa.keys())-1)
+    random_key = list(qa.keys())[random_num]
+    return str(random_key)
 
 main()
